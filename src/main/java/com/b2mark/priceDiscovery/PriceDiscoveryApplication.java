c7e885cbece9ec1c11cd8babe52a0770e6b1677a
Administrator <alkhandani@gmail.com>
2018-08-28 19:44:56 +0430

    Initial commit

---
+++ src/main/java/com/b2mark/priceDiscovery/PriceDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.b2mark.priceDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceDiscoveryApplication.class, args);
+	}
+}
