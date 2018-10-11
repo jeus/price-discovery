c9f9dee4ee7dcfefa94554118f588a880d1b3825
Administrator <alkhandani@gmail.com>
2018-10-11 17:25:29 +0330

    add test net btc for chaking network.

--- src/main/java/com/b2mark/priceDiscovery/controller/PriceController.java
+++ src/main/java/com/b2mark/priceDiscovery/controller/PriceController.java
@@ -89,7 +89,10 @@ public class PriceController {
         }
         return null;
     }
-
+    @GetMapping("/tbtcirr")
+    public Price getTbtcRial() {
+       return getBtcRial();
+    }
 
     @GetMapping("/btcirr")
     public Price getBtcRial() {
